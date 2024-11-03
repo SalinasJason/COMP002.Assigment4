bd42e8afac6d6def30ae3cb9668f723d3d842b75
SalinasJason <jasonsalinas770@gmail.com>
2024-11-03 13:55:14 -0800

    added answer number 2

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ function min(num1, num2) {
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
 
+function calculateCircumference(radius) {
+    const pi = 3.14;
+    return 2 * pi * radius;
+  }
+  
+  console.log(calculateCircumference(5)); // Output 31.4
+  
+
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
