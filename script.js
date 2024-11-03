ae3c0a191190342a66ff82b1e6a4864c73dffa05
SalinasJason <jasonsalinas770@gmail.com>
2024-11-03 13:29:58 -0800

    added answer number 1

--- script.js
+++ script.js
@@ -3,6 +3,15 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
+function min(num1, num2) {
+    return num1 < num2 ? num1 : num2;
+  }
+  
+ 
+  console.log(min(2, 8)); // Outputs: 2
+  console.log(min(6, 9));  // Outputs: 6
+  
+
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
